f93d3684302bcf95ab803514bd2d76ea7e72d9af
BeluchyGracia <beluchygracia64@gmail.com>
2024-04-01 12:45:52 -0700

    first

--- app/src/main/java/com/example/flashcard/AddCardActivity.kt
+++ app/src/main/java/com/example/flashcard/AddCardActivity.kt
@@ -10,19 +10,19 @@ import android.widget.ImageView
 class AddCardActivity : AppCompatActivity() {
     private lateinit var questionText: EditText
     private lateinit var answer: EditText
-    private lateinit var answer1: EditText
     private lateinit var answer2: EditText
     private lateinit var answer3: EditText
+    private lateinit var flashcardDatabase: FlashcardDatabase
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_card)
-
+        flashcardDatabase = FlashcardDatabase(this)
         // Initialisation des vues
         questionText = findViewById(R.id.questions)
         answer = findViewById(R.id.answer)
-        answer1 = findViewById(R.id.answer1)
         answer2 = findViewById(R.id.answer2)
         answer3 = findViewById(R.id.answer3)
         val saveBtn = findViewById<ImageView>(R.id.sbtn)
@@ -30,14 +30,12 @@ class AddCardActivity : AppCompatActivity() {
         // Récupération des données reçues depuis MainActivity
         val receivedQuestion = intent.getStringExtra("question")
         val receivedAnswer = intent.getStringExtra("answer1")
-        val receivedAnswer1 = intent.getStringExtra("answer2")
-        val receivedAnswer2 = intent.getStringExtra("answer3")
-        val receivedAnswer3 = intent.getStringExtra("answer4")
+        val receivedAnswer2 = intent.getStringExtra("answer2")
+        val receivedAnswer3 = intent.getStringExtra("answer3")
 
         // Affichage des données dans les EditText
         questionText.setText(receivedQuestion)
         answer.setText(receivedAnswer)
-        answer1.setText(receivedAnswer1)
         answer2.setText(receivedAnswer2)
         answer3.setText(receivedAnswer3)
 
@@ -45,17 +43,23 @@ class AddCardActivity : AppCompatActivity() {
         saveBtn.setOnClickListener {
             val question = questionText.text.toString()
             val answer = answer.text.toString()
-            val answer1 = answer1.text.toString()
             val answer2 = answer2.text.toString()
             val answer3 = answer3.text.toString()
+            val uuidQuestion = intent.getIntExtra("uuid", -1)
+
+            val newFlashCard = Flashcard(question, answer, answer2, answer3)
+            if (uuidQuestion != -1){
+                newFlashCard.uuid = uuidQuestion
+                flashcardDatabase.updateCard(newFlashCard)
+            }
+            flashcardDatabase.insertCard(newFlashCard)
 
             // Création de l'Intent de résultat pour renvoyer les données à MainActivity
             val resultIntent = Intent().apply {
                 putExtra("question", question)
                 putExtra("answer1", answer)
-                putExtra("answer2", answer1)
-                putExtra("answer3", answer2)
-                putExtra("answer4", answer3)
+                putExtra("answer2", answer2)
+                putExtra("answer3", answer3)
             }
             setResult(Activity.RESULT_OK, resultIntent)
 
--- app/src/main/java/com/example/flashcard/MainActivity.kt
+++ app/src/main/java/com/example/flashcard/MainActivity.kt
@@ -7,68 +7,56 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.ImageView
-import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.material.snackbar.Snackbar
+import kotlin.math.max
 
 class MainActivity : AppCompatActivity() {
     private val ADD_CARD_REQUEST = 1
     private val SAVE_CARD_REQUEST = 2
-
-    private lateinit var flashcardQuestion: TextView
-    private lateinit var flashcardAnswer: TextView
-    private  lateinit var textView2: TextView
-    private  lateinit var textView3: TextView
-    private lateinit var  textView4: TextView
-
-    private lateinit var bontonPlus: ImageView
-    private lateinit var boutonEdit: ImageView
-
-    private val resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-        val data: Intent? = result.data
-        if (result.resultCode == Activity.RESULT_OK && data != null) {
-            val question = data.getStringExtra("question")
-            val answer = data.getStringExtra("answer1")
-            val answer2 = data.getStringExtra("answer2")
-            val answer3 = data.getStringExtra("answer3")
-            val answer4 = data.getStringExtra("answer4")
-
-            flashcardQuestion.text = question
-            flashcardAnswer.text = answer
-            textView2.text = answer2
-            textView3.text = answer3
-            textView4.text = answer4
-        } else {
-            Log.i("AddCardActivity", "Save operation cancelled or no data returned")
-        }
-    }
+    private lateinit var flashcardDatabase: FlashcardDatabase
+    private var allFlashcards = mutableListOf<Flashcard>()
+    private var currentCardDisplayedIndex= 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        flashcardQuestion = findViewById(R.id.flashcard_question)
-        flashcardAnswer = findViewById(R.id.flashcard_answer)
-        textView2 = findViewById(R.id.textView2)
-        textView3 = findViewById(R.id.textView3)
-        textView4 = findViewById(R.id.textView4)
+        var flashcardQuestion = findViewById<TextView>(R.id.flashcard_question)
+        var flashcardAnswer = findViewById<TextView>(R.id.flashcard_answer)
+        var flashcardAnswer2 = findViewById<TextView>(R.id.flashcard_answer2)
+        var flashcardAnswer3 = findViewById<TextView>(R.id.flashcard_answer3)
 
-        bontonPlus = findViewById(R.id.pbtn)
-        boutonEdit = findViewById(R.id.editBtn)
+        var bontonPlus = findViewById<View>(R.id.pbtn)
+        var boutonEdit = findViewById<View>(R.id.editBtn)
+        var botnNext= findViewById<ImageView>(R.id.rightbtn)
+        var deletebtn=findViewById<ImageView>(R.id.poubtn)
 
-        flashcardAnswer.visibility = View.INVISIBLE
 
-        flashcardQuestion.setOnClickListener {
-            flashcardQuestion.visibility = View.INVISIBLE
-            flashcardAnswer.visibility = View.VISIBLE
+        flashcardDatabase = FlashcardDatabase(this)
+        flashcardDatabase.initFirstCard()
+        allFlashcards = flashcardDatabase.getAllCards().toMutableList()
+
+        if (allFlashcards.size > 0) {
+            flashcardQuestion.text = allFlashcards[0].question
+            flashcardAnswer.text = allFlashcards[0].answer
+            flashcardAnswer2.text = allFlashcards[0].wrongAnswer1
+            flashcardAnswer3.text = allFlashcards[0].wrongAnswer2
+
         }
 
-        flashcardAnswer.setOnClickListener {
-            flashcardAnswer.visibility = View.INVISIBLE
-            flashcardQuestion.visibility = View.VISIBLE
-        }
-
-        flashcardQuestion.visibility = View.VISIBLE
+//        flashcardAnswer.visibility = View.INVISIBLE
+//
+//        flashcardQuestion.setOnClickListener {
+//            flashcardQuestion.visibility = View.INVISIBLE
+//            flashcardAnswer.visibility = View.VISIBLE
+//        }
+//
+//        flashcardAnswer.setOnClickListener {
+//            flashcardAnswer.visibility = View.INVISIBLE
+//            flashcardQuestion.visibility = View.VISIBLE
+//        }
 
         bontonPlus.setOnClickListener {
             val intent = Intent(this, AddCardActivity::class.java)
@@ -76,37 +64,111 @@ class MainActivity : AppCompatActivity() {
         }
 
         boutonEdit.setOnClickListener {
-            val question = flashcardQuestion.text.toString()
-            val answer1 = flashcardAnswer.text.toString()
-            val answer2 = textView2.text.toString()
-            val answer3 = textView3.text.toString()
-            val answer4 = textView4.text.toString()
+            val (question, answer, wrongAnswer1, wrongAnswer2, uuid) = allFlashcards[currentCardDisplayedIndex]
 
             val intent = Intent(this, AddCardActivity::class.java)
-            intent.putExtra("question", question)
-            intent.putExtra("answer1", answer1)
-            intent.putExtra("answer2", answer2)
-            intent.putExtra("answer3", answer3)
-            intent.putExtra("answer4", answer4)
 
+            intent.putExtra("question", question)
+            intent.putExtra("answer1", answer)
+            intent.putExtra("answer2", wrongAnswer1)
+            intent.putExtra("answer3", wrongAnswer2)
+            intent.putExtra("uuid", uuid)
             startActivityForResult(intent, ADD_CARD_REQUEST)
+        }
+
+        botnNext.setOnClickListener {
+            if (allFlashcards.isEmpty()) {
+                return@setOnClickListener
+            }
+
+            currentCardDisplayedIndex = getRandomNumber(0,allFlashcards.size-1)
+
+            if (currentCardDisplayedIndex >= allFlashcards.size) {
+                currentCardDisplayedIndex = 0
+               // Snackbar.make(findViewById(R.id.editBtn), "No Cards", Snackbar.LENGTH_SHORT).show()
+
+            }
+
+            val (question, answer,wrongAnswer1,wrongAnswer2) = allFlashcards[currentCardDisplayedIndex]
+
+            flashcardQuestion.text = question
+            flashcardAnswer.text = answer
+            flashcardAnswer2.text = wrongAnswer1
+            flashcardAnswer3.text = wrongAnswer2
+        }
+
+        deletebtn.setOnClickListener {
+            val currentQuestion = flashcardQuestion.text.toString()
+            flashcardDatabase.deleteCard(currentQuestion)
+
+            allFlashcards = flashcardDatabase.getAllCards().toMutableList()
+
+            // Vérifier s'il reste des cartes
+            if (allFlashcards.isNotEmpty()) {
+                // Afficher la carte précédente (si disponible)
+                currentCardDisplayedIndex = max(0, currentCardDisplayedIndex - 1)
+                val (question, answer,wrongAnswer1,wrongAnswer2) = allFlashcards[currentCardDisplayedIndex]
+                flashcardQuestion.text = question
+                flashcardAnswer.text = answer
+                flashcardAnswer2.text = wrongAnswer1
+                flashcardAnswer3.text = wrongAnswer2
+
+            } else {
+                // S'il n'y a plus de cartes, afficher un état vide
+                flashcardQuestion.text = ""
+                flashcardAnswer.text = ""
+            }
+        }
+    }
+
+    fun getRandomNumber(minNumber: Int, maxNumber: Int): Int {
+        return (minNumber..maxNumber).random() // generated random from 0 to 10 included
+    }
+
+    val resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        val data: Intent? = result.data
+        if (data != null) { // Check that we have data returned
+            val question = data.getExtras()?.getString("question")
+            val answer = data.getExtras()?.getString("answer1")
+            val answer2 = data.getExtras()?.getString("answer2")
+            val answer3 = data.getExtras()?.getString("answer3")
+
+
+            // Log the value of the strings for easier debugging
+            Log.i("MainActivity", "question: $question")
+            Log.i("MainActivity", "answer: $answer")
+            Log.i("MainActivity", "answer: $answer2")
+            Log.i("MainActivity", "answer: $answer3")
+
+
+            // Display newly created flashcard
+            findViewById<TextView>(R.id.flashcard_question).text = question
+            findViewById<TextView>(R.id.flashcard_answer).text = answer
+            findViewById<TextView>(R.id.flashcard_answer2).text = answer2
+            findViewById<TextView>(R.id.flashcard_answer3).text = answer3
+
+        } else {
+            Log.i("MainActivity", "Returned null data from AddCardActivity")
         }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
+        var flashcardQuestion = findViewById<TextView>(R.id.flashcard_question)
+        var flashcardAnswer = findViewById<TextView>(R.id.flashcard_answer)
+        var flashcardAnswer2 = findViewById<TextView>(R.id.flashcard_answer2)
+        var flashcardAnswer3 = findViewById<TextView>(R.id.flashcard_answer3)
+
         if (requestCode == ADD_CARD_REQUEST && resultCode == Activity.RESULT_OK) {
             val question = data?.getStringExtra("question")
             val answer = data?.getStringExtra("answer1")
-            val answer1 = data?.getStringExtra("answer2")
-            val answer2 = data?.getStringExtra("answer3")
-            val answer3 = data?.getStringExtra("answer4")
+            val answer2 = data?.getStringExtra("answer2")
+            val answer3 = data?.getStringExtra("answer3")
 
             flashcardQuestion.text = question
             flashcardAnswer.text = answer
-            textView2.text = answer1
-            textView3.text = answer2
-            textView4.text = answer3
+            flashcardAnswer2.text = answer2
+            flashcardAnswer3.text = answer3
 
         }
     }
